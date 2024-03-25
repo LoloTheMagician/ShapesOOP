ba1b159b66bb473f456f2737198bd5860be40850
loloTheMagician <elperiplodelolo@gmail.com>
2024-03-25 11:29:03 +0100

    Agregamos un método que nos permite cambiar el tipo de Ataque

--- Habitantes/Animal.ts
+++ Habitantes/Animal.ts
@@ -4,15 +4,20 @@ import { Say } from './Say.ts'
 import { Attack } from './Attack.ts'
 
 abstract class Animal extends Habitante {
-    attackBehaviour: Attack;
+    private attackBehaviour: Attack;
 
     constructor(name: string, sex: Sex, numberOfPawns: number, says: Say, attackBehaviour: Attack) {
         super(name, sex, numberOfPawns, says)
         this.attackBehaviour = attackBehaviour
     }
 
+
+    public set setAttackBehaviour(newAttackBehaviour: Attack) {
+        this.attackBehaviour = newAttackBehaviour
+    }
+
     public toAttack(enemy: Animal) {
-        enemy.setLife = enemy.getLife - this.attackBehaviour.damage
+        enemy.setLife = enemy.getLife - this.attackBehaviour.getDamage
     }
 
 }
