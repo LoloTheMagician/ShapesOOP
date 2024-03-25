e9b4bebda077cf868df57881ec0b266d06c75bfb
loloTheMagician <elperiplodelolo@gmail.com>
2024-03-25 11:29:26 +0100

    Podemos pasar como parámetro el tipo de Ataque

--- Habitantes/Cat.ts
+++ Habitantes/Cat.ts
@@ -1,10 +1,10 @@
 import { Sex } from './types.ts'
 import { CatSay } from "./Say.ts"
 import { Animal } from "./Animal.ts"
-import { Attack } from "./Attack.ts";
+import { Attack, BaseAttack } from "./Attack.ts";
 
 class Cat extends Animal {
-    constructor(name: string, sex: Sex, amountOfPawns: number, attackBehaviour : Attack = new Attack()) {
+    constructor(name: string, sex: Sex, amountOfPawns: number, attackBehaviour: Attack = new BaseAttack()) {
         super(name, sex, amountOfPawns, new CatSay(), attackBehaviour)
     }
 }
--- Habitantes/Dog.ts
+++ Habitantes/Dog.ts
@@ -1,11 +1,11 @@
 import { Animal } from "./Animal.ts";
-import { Attack } from "./Attack.ts";
+import { Attack, BaseAttack } from "./Attack.ts";
 import { CanineSay } from "./Say.ts";
 import { Sex } from './types.ts';
 
 class Dog extends Animal {
 
-    constructor(name: string, sex: Sex, amountOfPaws: number, attackBehaviour: Attack = new Attack) {
+    constructor(name: string, sex: Sex, amountOfPaws: number, attackBehaviour: Attack = new BaseAttack()) {
         super(name, sex, amountOfPaws, new CanineSay(), attackBehaviour)
     }
 
