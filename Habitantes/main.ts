639d3815584fc9f3878d6a99faa22332426412da
loloTheMagician <elperiplodelolo@gmail.com>
2024-03-25 11:06:38 +0100

    Perro ataca a gato

--- Habitantes/main.ts
+++ Habitantes/main.ts
@@ -1,6 +1,7 @@
 import { Dog } from "./Dog.ts";
 import { Cat } from "./Cat.ts";
 import { CatWoman } from './CatWoman.ts'
+import { ClawAttack } from './Attack.ts'
 
 const mujerGato = new CatWoman('Gatubela', 2, 2)
 console.log(
@@ -8,11 +9,13 @@ console.log(
 )
 
 const gato = new Cat('Nieves', 'Female', 4)
-const perro = new Dog('Tobby', 'Male', 4)
+const perro = new Dog('Tobby', 'Male', 4, new ClawAttack())
 
 console.log(gato.displayLife())
 perro.toAttack(gato)
 console.log(gato.displayLife())
+perro.toAttack(perro)
+console.log(perro.displayLife())
 
 
 
